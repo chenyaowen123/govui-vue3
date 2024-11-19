e2b1756bbf2fe1ed2ef2d0984fec0079b831cc70
陈耀文 <chenyaowen@fengtiaolishun.com>
2024-11-19 09:53:09 +0800

    样式统一

--- packages/components/datepicker/datepicker.vue
+++ packages/components/datepicker/datepicker.vue
@@ -76,17 +76,17 @@ const innerDisabled = computed(() => {
 });
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .gov-datepicker {
 	display: inline-block;
 	width: 100%;
-	:deep(.dp__input_wrap) {
+	.dp__input_wrap {
 		svg {
 			fill: currentColor;
 		}
 	}
 
-	:deep(.dp__main *) {
+	.dp__main * {
 		font-size: 14px;
 	}
 
@@ -112,48 +112,27 @@ const innerDisabled = computed(() => {
 	}
 }
 
-.dp__theme_dark {
-	// dark theme
-	--dp-background-color: #212121;
-	--dp-text-color: #ffffff;
-	--dp-hover-color: #484848;
-	--dp-hover-text-color: #ffffff;
-	--dp-hover-icon-color: #959595;
-	--dp-primary-color: #005cb2;
-	--dp-primary-text-color: #ffffff;
-	--dp-secondary-color: #a9a9a9;
-	--dp-border-color: #2d2d2d;
-	--dp-menu-border-color: #2d2d2d;
-	--dp-border-color-hover: #aaaeb7;
-	--dp-disabled-color: #737373;
-	--dp-scroll-bar-background: #212121;
-	--dp-scroll-bar-color: #484848;
-	--dp-success-color: #00701a;
-	--dp-success-color-disabled: #428f59;
-	--dp-icon-color: #959595;
-	--dp-danger-color: #e53935;
-	--dp-highlight-color: rgba(0, 92, 178, 0.2);
-}
+.dp__theme_dark,
 .dp__theme_light {
 	// light theme
 	--dp-background-color: #ffffff;
-	--dp-text-color: #212121;
-	--dp-hover-color: #f3f3f3;
-	--dp-hover-text-color: #212121;
-	--dp-hover-icon-color: #959595;
-	--dp-primary-color: #1976d2;
-	--dp-primary-text-color: #f8f5f5;
+	--dp-text-color: var(--gov-text-color);
+	--dp-hover-color: var(--gov-fill-color);
+	--dp-hover-text-color: var(--gov-text-color);
+	--dp-hover-icon-color: var(--gov-text-color);
+	--dp-primary-color: var(--gov-primary);
+	--dp-primary-text-color: #fff;
 	--dp-secondary-color: #c0c4cc;
-	--dp-border-color: #ddd;
-	--dp-menu-border-color: #ddd;
-	--dp-border-color-hover: #aaaeb7;
-	--dp-disabled-color: #f6f6f6;
+	--dp-border-color: var(--gov-border-color);
+	--dp-menu-border-color: var(--gov-border-color);
+	--dp-border-color-hover: var(--gov-primary);
+	--dp-disabled-color: var(--gov-fill-color-5);
 	--dp-scroll-bar-background: #f3f3f3;
 	--dp-scroll-bar-color: #959595;
-	--dp-success-color: #76d275;
-	--dp-success-color-disabled: #a3d9b1;
-	--dp-icon-color: #959595;
-	--dp-danger-color: #ff6f60;
-	--dp-highlight-color: rgba(25, 118, 210, 0.1);
+	--dp-success-color: var(--gov-success);
+	--dp-success-color-disabled: var(--gov-success-5);
+	--dp-icon-color: var(--gov-text-color);
+	--dp-danger-color: var(--gov-danger);
+	--dp-highlight-color: var(--gov-primary);
 }
 </style>
